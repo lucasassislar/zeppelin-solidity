c6bde0281b6c9a320e99f7080bcf36054e222e76
Lucas Assis <lucasassis@outlook.com>
2017-10-07 17:06:11 -0300

    - LimitedTokenCrowdsale.sol: * Added a tracker for all the tokens * Added a counter for the tokens * Fixed buying tokens * Fixed withdrwaing tokens * Added function for withdrwaing everything to wallet

--- contracts/crowdsale/LimitedTokenCrowdsale.sol
+++ contracts/crowdsale/LimitedTokenCrowdsale.sol
@@ -4,6 +4,7 @@ import '../token/StandardToken.sol';
 import '../math/SafeMath.sol';
 
 contract LimitedTokenCrowdSale {
+    // safemath for the .add, .mul used everywhere that deals with tokens/eth
     using SafeMath for uint256;
 
     // The token being sold
@@ -12,12 +13,8 @@ contract LimitedTokenCrowdSale {
     // safe mode (if an integrity check failed, we give control to the organization)
     //bool public safeMode;
 
-    // the total amount of tokens we have to supply
-    //uint256 public tokenSupply;
-
-    // start and end timestamps where investments are allowed (both inclusive)
-    //uint256 public startTime;
-    //uint256 public endTime;
+    // address where tokens balances are stored (for user withdrawal)
+    mapping(address => uint256) public tokenBalance;
 
     // address where funds are collected
     address public wallet;
@@ -28,6 +25,9 @@ contract LimitedTokenCrowdSale {
     // amount of raised money in wei
     uint256 public weiRaised;
 
+    // amount of total tokens sold
+    uint256 public tokensSold;
+
     /**
     * event for token purchase logging
     * @param purchaser who paid for the tokens
@@ -37,17 +37,21 @@ contract LimitedTokenCrowdSale {
     */
     event TokenPurchase(address indexed purchaser, address indexed beneficiary, uint256 value, uint256 amount);
 
-
   function LimitedTokenCrowdSale(uint256 _rate, address _tokenAddress, address _wallet) {
-    require(_rate > 0);
-    require(_tokenAddress != 0x0);
+      require(_rate > 0);
+      require(_tokenAddress != 0x0);
 
-    rate = _rate;
-    token = StandardToken(_tokenAddress);
-    wallet = _wallet;
+      rate = _rate;
+      token = StandardToken(_tokenAddress);
+      wallet = _wallet;
   }
 
   function () payable {
+    buyToken();
+  }
+
+  // Saves how much the user bought in tokens
+  function buyToken() payable {
     require(msg.sender != 0x0);
     require(msg.value != 0);
 
@@ -55,23 +59,37 @@ contract LimitedTokenCrowdSale {
     address sender = msg.sender;
 
     // calculate token amount to be sold
-    uint256 tokens = weiAmount.mul(rate);
+    uint256 tokens = weiAmount / rate;
 
     // must have enough tokens to sell
     require(token.balanceOf(this) > tokens); 
-    forwardTokens(tokens, msg.sender);
-    forwardFunds(weiAmount);
+    
+    tokenBalance[sender] = tokenBalance[sender].add(tokens);
+
+    // update how much wei we have raised
+    weiRaised = weiAmount.add(weiRaised);
+    tokensSold = tokensSold.add(tokens);
+
     TokenPurchase(msg.sender, sender, weiAmount, tokens);
-
-    // update state
-    weiRaised = weiRaised.add(weiAmount);
   }
 
-  function forwardTokens(uint256 tokenAmount, address sender) internal {
-    token.transfer(sender, tokenAmount);
+  // Withdraws the tokens that the sender owns
+  function withdrawTokens() public {
+    uint256 balance = tokenBalance[msg.sender];
+    require(balance > 0);
+
+    tokenBalance[msg.sender] = 0;
+    if (!token.transfer(msg.sender, balance))
+    {
+      tokenBalance[msg.sender] = balance;
+    }
   }
 
-  function forwardFunds(uint256 weiAmount) internal {
-    wallet.transfer(weiAmount);
+  // Send all the funds currently in the wallet to 
+  // the organization wallet provided at the contract creation
+  function withdrawFunds() public {
+    require(this.balance > 0);
+
+    wallet.transfer(this.balance);
   }
 }
